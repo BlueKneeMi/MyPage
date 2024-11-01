a0c9866f1a06531c96fb503999b71473c683cb37
BlueKneeMi <smiryang@gmail.com>
2024-11-01 17:40:13 +0100

    Some fixes so it deploys on github

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import 'lenis/dist/lenis.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </StrictMode>,
 )
+a
